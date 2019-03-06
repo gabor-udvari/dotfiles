f65a2284d68976ecd87375a1003adf94113d3f7b
Gábor Udvari <gabor.udvari@gmail.com>
2019-03-06 13:23:50 +0100

    Release new version of maintain-torrents.sh

--- home/bin/maintain-torrents.sh
+++ home/bin/maintain-torrents.sh
@@ -57,25 +57,61 @@ function get_torrent_names {
   echo "$torrents" | sed -e 's/[ ]\{2,11\}/;/g' -e 's/^;//' | cut -d ';' -f "$field_id"
 }
 
+function get_torrent_ids {
+  # get the ids of the torrents
+  torrent_ids=$(transmission-remote --auth "$AUTH" -l | grep Done | awk '{print $1}')
+
+  echo "$torrent_ids"
+}
+
+function get_torrent_files {
+  get_torrent_ids | while read t; do
+    # What this sed does:
+    # 1. Get both info and files output for each torrent
+    # 2. Hold the "Location:" part
+    # 3. Print out the "Location:" part before every filename
+    # 4. Replace every 2nd newline with "||" so we can later cut it
+    transmission-remote --auth "$AUTH" -t "$t" -i -f | sed -n -e '/^ *Location: /h;x;s/^ *Location: //;x' -e '/^.*[0-9]:.*[kMG]B.*$/{x;p;x;s/^.*[kMG]B *\(.*\)$/\1/p}' | sed -e 'N;s/\n/||/g'
+  done
+}
+
+function get_torrent_directories {
+  # The output has || separating location and files inside the torrent
+  get_torrent_files | sed -n 's#^\(.*||[^/]*\)/.*$#\1#p' | sort -n | uniq
+}
+
 function get_additional_files {
-  echo "== Files in public folder but not in torrent list ===="
+  echo "== Files on the filesystem but not in the torrent list ===="
 
-  # get list of torrents
-  list_torrents=$(get_torrent_names)
-  if [ "$?" -ne "0" ]; then
-    echo "$list_torrents"
-    exit 1
-  fi
+  # The output has || separating location and files inside the torrent
+  torrents="$(get_torrent_directories)"
 
-  files_public=$(ls -1 /var/downloads/public)
+  torrent_locations="$(echo "$torrents" | sed -n 's/^\(.*\)||.*$/\1/p' | sort -n | uniq)"
 
-  while read torrent; do
-    list_temp=$(echo "$files_public" | grep -v -F "$torrent")
-    files_public=$list_temp
-  done <<<"$list_torrents"
+  # Make temporary files
+  file_torrents="$(mktemp)"
+  file_filesystem="$(mktemp)"
+  file_diff="$(mktemp)"
 
-  echo "$files_public"
+  # Loop through every location, and get all the files into a list
+  echo "$torrent_locations" | while read l; do
+    # Find the directories 1 folder deep
+    find "$l" -mindepth 1 -maxdepth 1 -type d | sort -n >>"$file_filesystem"
+  done
+
+  sort -n "$file_filesystem" | uniq >"$file_diff"
+  mv "$file_diff" "$file_filesystem"
+
+  # Replace || back to /, and squeeze any //
+  echo "$torrents" | grep -F "$l" | sed 's/||/\//' | tr -s '/' | sort -n >"$file_torrents"
+
+  diff "$file_torrents" "$file_filesystem" | sed -n 's#^> \(/.*\)$#\1#p'
+}
+
+function cleanup {
+  rm "$file_torrents" "$file_filesystem"
 }
 
 get_args $@
 get_additional_files
+cleanup
