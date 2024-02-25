1b36513fb7f477f7ffb729d0c44bd591277c9a8e
Gábor Udvari <gabor.udvari@gmail.com>
2024-02-25 22:03:02 +0100

    Switch to emacs-pgtk, update hledger config

--- manifest.scm
+++ manifest.scm
@@ -3,5 +3,5 @@
 ;; that accepts a '--manifest' (or '-m') option.
 
 (specifications->manifest (list "make"
-                                "emacs"
+                                "emacs-pgtk"
                                 ))
