38e47ea50d505511ed512828fad14f54385e9602
Gábor Udvari <gabor.udvari@gmail.com>
2024-09-17 21:47:21 +0200

    Update channels, add Git config

--- channels.scm
+++ channels.scm
@@ -3,10 +3,10 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "71f0676a295841e2cc662eec0d3e9b7e69726035")
+          "871a744e976a70480b8f2040400b00e9bd5a7a79")
         (introduction
           (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            "df14cacca9b95d69fdd3b6191e42df36af266bcd"
             (openpgp-fingerprint
               "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
@@ -25,7 +25,7 @@
         (url "https://gitlab.inria.fr/guix-hpc/guix-past")
         (branch "master")
         (commit
-          "aab6148759476f0270bbe098919a978da45d7a2d")
+          "792d830606345bddf5712252a1ed4fd87ee55c42")
         (introduction
           (make-channel-introduction
             "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
@@ -36,7 +36,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "32cc099af9be99bd59c4b01e6292ab574ed0b1ab")
+          "99a47731552b179ea021a65a8a6f474cece68182")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
