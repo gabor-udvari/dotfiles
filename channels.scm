a7e7805293b8330d50d90c5daa2efbfb29a324a6
Gábor Udvari <gabor.udvari@gmail.com>
2025-06-15 22:32:30 +0200

    Update channels

--- channels.scm
+++ channels.scm
@@ -4,7 +4,7 @@
         (url "https://codeberg.org/guix/guix.git")
         (branch "master")
         (commit
-          "027a47787f8dcf6651a1c20c5b475376defe6d6b")
+          "6c980300f485953bb4935abc2ea0047118e07466")
         (introduction
           (make-channel-introduction
             "df14cacca9b95d69fdd3b6191e42df36af266bcd"
@@ -15,7 +15,7 @@
         (url "https://gitlab.com/guix-gaming-channels/games.git")
         (branch "master")
         (commit
-          "d5951ea89b13e07b5c44f4827b2330d2ea9065d4")
+          "9f0daa2b1bd0dbfe634bb07398a8daf8b026adf3")
         (introduction
           (make-channel-introduction
             "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
@@ -38,19 +38,33 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "02270b585e7d641afabd86ee6eaf7a6aad2f5df7")
+          "473e5a62cd512cc0d5b4ac2c33be0bc0d0379435")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-       ;;; TODO: once https://issues.guix.gnu.org/76081 is merged, we can remove this
-       (name 'gocix)
+        ;; TODO: once https://issues.guix.gnu.org/76081 is merged, we can remove this
+        (name 'gocix)
         (url "https://github.com/fishinthecalculator/gocix")
         (branch "main")
+        (commit
+          "46429d2fc4a428ee52853228107abbc5e3ee6d81")
         (introduction
-         (make-channel-introduction
-          "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
-          (openpgp-fingerprint
-           "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2")))))
+          (make-channel-introduction
+            "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
+            (openpgp-fingerprint
+              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+      (channel
+        ;; This is a dependency for the gocix channel
+        (name 'sops-guix)
+        (url "https://github.com/fishinthecalculator/sops-guix")
+        (branch "main")
+        (commit
+          "8005f05fb1790cad007df3835798f4563e5ca550")
+        (introduction
+          (make-channel-introduction
+            "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+            (openpgp-fingerprint
+              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2")))))
